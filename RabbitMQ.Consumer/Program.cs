dab373d5e637756d99d5b6fd966f1372ecc8e3f7
mhmtyldz <yldzmahmut0@gmail.com>
2021-02-14 16:40:03 +0300

    Add project files.

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMQ.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" }; // Rabbitmq clientına bağlanmak için bir tane factory oluşturuyorum.
+            //factory.Uri = new Uri(""); // Burası bağlanacağım url ben locale bağlandım
+            //Bağlantı açıyorum alttaki kodda
+            using (var connection = factory.CreateConnection())
+            {
+                //kanal oluşturmam gerekiyor ve kanal oluşturma kodları
+                using (var channel = connection.CreateModel())
+                {
+                    //Publisher da kuyruğu nasıl oluşturduysak aynı şekilde burada da kuyruğu o şekilde oluşturmalıyız.
+                    //Queue methodunda belirttiğimiz methodların hepsi aynı olmak zorunda yoksa eşleşemedeği için
+                    //İşlemimiz gerçekleşmeyecektir
+                    channel.QueueDeclare(queue: "helloworld", durable: false, exclusive: false, arguments: null);
+
+                    //Kuyruktaki mesajları alması için bir event oluşturuyorum. Ve dinleyeceği kanalı belirtiyorum.
+                    var consumer = new EventingBasicConsumer(channel); // Kanalı bu şekilde dinletiyorum.
+                    channel.BasicConsume(queue: "helloworld", true, consumer); //autoack true verirsek otomatik olarak doğru da yanlışta olsa kuyruktan sil. Eğer true dersek rabbitmq ya ben belirtcem sil diye 
+                    //Mesajı ne zaman yakalayacağım.Kuyruktaki mesajları alıyorum.
+                    consumer.Received += (model, ea) =>
+                    {
+                        var bodyByte = ea.Body.Span; //Kuyruktaki göndermiş olduğum mesajı alıyorum. String göndermiştim. Stringe çevirmeliyim. Eğerki model gönderseydim serilize deseriliaze yapmalıydım
+                        var message = Encoding.UTF8.GetString(bodyByte);
+                        Console.WriteLine("Mesaj alındı : " + message);
+                    };
+                }
+
+                Console.WriteLine("Çıkış yapmak için tıklayınız..");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,41 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMQ.Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" }; // Rabbitmq clientına bağlanmak için bir tane factory oluşturuyorum.
+            //factory.Uri = new Uri(""); // Burası bağlanacağım url ben locale bağlandım
+            //Bağlantı açıyorum alttaki kodda
+            using (var connection = factory.CreateConnection())
+            {
+                //kanal oluşturmam gerekiyor ve kanal oluşturma kodları
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "helloworld", durable: false, exclusive: false, arguments: null); //Kuyruk oluşturuyorum. ikinci parametrem durable, 
+                    //durable false ise rabbitmq restart atarsa mesajlarımın hepsi uçar.
+                    //(Memoryde duruyor şuan çünkü daha hızlı olması için) ama true dersek 
+                    //disktte tutcak ve hiçb irşekilde mesaklarım restart durumunda kaybolmicak
+                    //exclusive bu kuyruğa sadece 1 kanal mı yoksa başka kanallarda bağlanabilsin mi demek istiyoruz. False dersek diğerleri de bağlanır.
+                    //autodelete bir kuyrukta 20 mesaj var son mesajda kuyruktan çıkarsa bu kuyruk silinsin mi silinmesin mi demek istiyoruz. 
+                    //false silinsin true silinmesin.
+                    string message = "Hello World";
+                    //Mesajlarımı byte olarak göndermemiz lazım. Word pdf image text herşeyi byte çevirip gönderebilieceğimiz için stringi byte a çevircem
+                    var bodyByte = Encoding.UTF8.GetBytes(message);
+
+                    //Alt satırdaki kodda mesajı kuyruğuma gönderiyorum.
+                    channel.BasicPublish("", routingKey: "helloworld", null, body: bodyByte);//exchange kullanmadığımız için boş geçiyorum default exchange geçsin.
+
+                    Console.WriteLine("Mesajınız Gönderilmiştir.");
+                }
+
+                Console.WriteLine("Çıkış yapmak için tıklayınız..");
+                Console.ReadLine();
+            }
+        }
+    }
+}
