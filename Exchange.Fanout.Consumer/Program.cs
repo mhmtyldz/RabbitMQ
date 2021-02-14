4b6018d1c4115b76e985f29dea48ab7d28b874b3
mhmtyldz <yldzmahmut0@gmail.com>
2021-02-14 19:17:54 +0300

    Fanout Exhange 'i oluşturuldu ve buna bağlı işlemler yapıldı

---
+++ Exchange.Fanout.Consumer/Program.cs
@@ -0,0 +1,63 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace Exchange.Fanout.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://nyvihzls:rLqUu-fCPotv9ojMMNnrr7mbOH9oKFLS@eagle.rmq.cloudamqp.com/nyvihzls");
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    //publisher tarafta yaptığı exchanger işlemini burada da gerçekleştiriyorum.
+                    //channel.QueueDeclare(queue: "task_queue", durable: true, exclusive: false, arguments: null);
+                    channel.ExchangeDeclare(exchange: "logs", durable: true, type: ExchangeType.Fanout);
+
+                    //herbir instance ı ayağa kaldırdığımda kuyruk isimlerinin farklı olmasını istiyorum
+                    //aşağıya kuyruk ismi oluşturuyorum
+
+                    var queueName = channel.QueueDeclare().QueueName;
+
+                    channel.QueueBind(queueName, exchange: "logs", routingKey: string.Empty);
+
+
+
+                    channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+                    Console.WriteLine("logları bekliyorum.......");
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    channel.BasicConsume(queue: queueName, autoAck: false, consumer);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var bodyByte = ea.Body.Span;
+                        var log = Encoding.UTF8.GetString(bodyByte);
+
+                        Console.WriteLine("Log alındı : " + log);
+                        int time = int.Parse(GetMessage(args));
+
+                        Thread.Sleep(time);
+                        Console.WriteLine("Loglama işlemi sona erdi...");
+
+
+                        channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+
+                    };
+                    Console.WriteLine("Çıkış yapmak için tıklayınız..");
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return args[0];
+        }
+    }
+}
---
+++ Exhange.Fanout/Program.cs
@@ -0,0 +1,50 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Exhange.Fanout
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://nyvihzls:rLqUu-fCPotv9ojMMNnrr7mbOH9oKFLS@eagle.rmq.cloudamqp.com/nyvihzls");
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    //Artık ben direk queue ye göndermeyeceğim için (exchangeye göndercem)alttaki queueyi yorum satırına alıyorum
+                    //channel.QueueDeclare(queue: "task_queue", durable: true, exclusive: false, arguments: null);
+
+                    channel.ExchangeDeclare(exchange: "logs", durable: true, type: ExchangeType.Fanout);
+
+                    string message = GetMessage(args);
+                    for (int i = 1; i < 11; i++)
+                    {
+                        var bodyByte = Encoding.UTF8.GetBytes($"{message}-{i}");
+
+
+                        var properties = channel.CreateBasicProperties();
+                        properties.Persistent = true;
+                        //Yorum satırında bulunan basicpublish methodunda ilk örnekte excahnge belirtmiyordum artık belirtiyorum
+                        //routingkeyi de siliyorum.
+                        //channel.BasicPublish("", routingKey: "task_queue", basicProperties: properties, body: bodyByte);
+                        channel.BasicPublish(exchange: "logs", routingKey: "", basicProperties: properties, body: bodyByte);
+
+                        Console.WriteLine("Mesajınız Gönderilmiştir.");
+                    }
+
+                }
+
+                Console.WriteLine("Çıkış yapmak için tıklayınız..");
+                Console.ReadLine();
+            }
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return args[0];
+        }
+    }
+}
